0336fb3cb0d91350ed85c5561e6d9f1310a706ae
Orumgbe <orumgbeugbekile@gmail.com>
2024-08-12 14:26:15 +0100

    register schema in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/streamslab')],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1/streamslab'),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
