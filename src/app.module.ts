9a7e6aee19bbc6f7b426a2b45c6124f0e7b476a8
Orumgbe <orumgbeugbekile@gmail.com>
2024-08-13 21:14:40 +0100

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
 import { Transaction, TransactionSchema } from './schemas/transaction.schema';
+import { UsersController } from './controllers/user.controller';
+import { TransactionsController } from './controllers/transaction.controller';
+import { UserService } from './services/user.service';
+import { TransactionService } from './services/transaction.service';
 
 @Module({
   imports: [
@@ -11,7 +15,7 @@ import { Transaction, TransactionSchema } from './schemas/transaction.schema';
       { name: Transaction.name, schema: TransactionSchema },
     ]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UsersController, TransactionsController],
+  providers: [UserService, TransactionService],
 })
 export class AppModule {}
